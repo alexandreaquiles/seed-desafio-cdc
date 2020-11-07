4c4b7e79738993fc4b745e006dd87da1854cf72a
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2020-11-07 10:41:31 -0300

    [refactoring] remove construtor e setters desnecessários

--- src/main/java/br/com/alexandreaquiles/casadocodigo/autor/Autor.java
+++ src/main/java/br/com/alexandreaquiles/casadocodigo/autor/Autor.java
@@ -27,11 +27,6 @@ public class Autor {
     @NotNull
     private LocalDateTime criadoEm = LocalDateTime.now();
 
-    @Deprecated
-    /** apenas para o amigo hibernate */
-    public Autor() {
-    }
-
     public Autor(String nome, String email, String descricao) {
         this.nome = nome;
         this.email = email;
@@ -42,39 +37,19 @@ public class Autor {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public String getNome() {
         return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
     }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
     public String getDescricao() {
         return descricao;
     }
 
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
     public LocalDateTime getCriadoEm() {
         return criadoEm;
-    }
-
-    public void setCriadoEm(LocalDateTime criadoEm) {
-        this.criadoEm = criadoEm;
     }
 }
